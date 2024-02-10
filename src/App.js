3944367ae8b494a805a2b4588826e0cc788c8b1a
Zufarbek <zufarbek.abdurakhmonov@gmail.com>
2024-02-10 11:53:16 +0500

    initail commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          1 - WorkShop
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://soffstudy.uz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Soffstudy.uz
         </a>
       </header>
     </div>
